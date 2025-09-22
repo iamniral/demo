2e70f2bb9a0cabc97b68b99457b77c1dbc467998
iamniral <niral.shah@acldigital.com>
2025-09-22 15:47:54 +0530

    Initial commit

---
+++ appiumtest/test_mirai.py
@@ -0,0 +1,17 @@
+import time
+from appium import webdriver
+
+
+
+desired_caps = dict(
+    deviceName='2a50ee87',
+    platformName='Android',
+    browserName='chrome',
+)
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+driver.get("http://www.google.com")
+print(driver.title)
+time.sleep(2)
+driver.quit()
