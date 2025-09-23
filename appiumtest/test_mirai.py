753252764e9db53fa33c0f5218b166dc435d64a9
iamniral <niral.shah@acldigital.com>
2025-09-23 18:19:06 +0530

    added calculator test

--- appiumtest/test_mirai.py
+++ appiumtest/test_mirai.py
@@ -9,16 +9,19 @@ options = AppiumOptions()
 options.set_capability('platformName', 'Android')
 options.set_capability('appium:platformVersion', '16.0')
 options.set_capability('appium:deviceName', 'emulator-5554')
-options.set_capability('appium:browserName', 'chrome')
+options.set_capability('appium:browserName', 'Chrome')
 options.set_capability('appium:automationName', 'UiAutomator2')
 # Add the capability to enable automatic Chromedriver download
 options.set_capability('appium:chromedriverAutodownload', True)
+options.set_capability('appium:skipDeviceInitialization', True)
+options.set_capability('appium:ignoreHiddenApiPolicyError', True)
+options.set_capability('appium:noReset', True)
 
 # Step 3: Connect to the Appium server and create a new session
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', options=options)
+driver = webdriver.Remote('http://127.0.0.1:4723', options=options)
 
 # Your test commands
-driver.get("https://www.google.com/")
+#driver.get("https://www.google.com/")
 print(driver.title)
 time.sleep(2)
 
