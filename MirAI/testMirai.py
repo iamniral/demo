d42fc8d56e8ea4e9b0fb0bb05d77d9324d8f9d21
iamniral <niral.shah@acldigital.com>
2025-10-01 15:47:56 +0530

    App Launch Successfull

--- MirAI/testMirai.py
+++ MirAI/testMirai.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.actions.pointer_actions import PointerInput
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions import interaction
-
+from selenium.webdriver.common.by import By
 
 # Step 1: Create an AppiumOptions object
 options = AppiumOptions()
@@ -26,7 +26,7 @@ options.set_capability('appium:app', 'C:/Users/Niral.Shah/Documents/Appium/apk/P
 # Step 3: Connect to the Appium server and create a new session
 driver = webdriver.Remote('http://localhost:4723', options=options)
 print("App has been installed and launched successfully!")
-time.sleep(10)
+time.sleep(15)
 
 # Get the device screen size
 screen_size = driver.get_window_size()
@@ -40,11 +40,22 @@ driver.execute_script('mobile: swipeGesture', {
     'top': 0,          # Start Y of the swipe area (top-left corner)
     'width': width,    # Width of the swipe area
     'height': height,  # Height of the swipe area
-    'direction': 'right', # Specify the direction
+    'direction': 'left', # Specify the direction
     'percent': 0.8     # Swipe dist   ance as a float between 0.0 and 1.0 (80%)
 })
 print("Swipe complete.")
 
+#Click on Get Started
+driver.find_element(By.ID, 'com.panasonic.in.miraie:id/button_get_started').click()
+time.sleep(5)
+driver.find_element(By.ID,'com.panasonic.in.miraie:id/panasonic_image').is_displayed()
+print("Panasonic Image is displayed")
+driver.find_element(By.ID, 'com.panasonic.in.miraie:id/enter_mobile_number').send_keys("shahniral01@gmail.com")
+driver.find_element(By.ID, 'com.panasonic.in.miraie:id/password_container').send_keys("Vasupujy@3")
+driver.find_element(By.ID, 'com.panasonic.in.miraie:id/loginButton').click()
+
 # End the driver session
 time.sleep(5)
-# driver.quit()
+driver.quit()
+
+driver.find_element(By.LINK_)
---
+++ SmartThings/App_Launch.py
@@ -0,0 +1,50 @@
+import time
+from appium import webdriver
+from appium.options.common import AppiumOptions
+from selenium.webdriver.common.by import By
+
+options = AppiumOptions()
+options.set_capability('platformName', 'Android')
+options.set_capability('appium:deviceName', 'emulator-5554')
+options.set_capability('appium:automationName', 'UiAutomator2')
+# options.set_capability('appium:app', 'C:/Users/Niral.Shah/Documents/Appium/apk/SmartThings.apk')
+
+# --- CLEANSED LAUNCH CAPABILITIES ---
+options.set_capability('appium:appPackage', 'com.samsung.android.oneconnect')
+
+# Use wildcard on appWaitActivity to successfully wait for any activity to load
+options.set_capability('appium:appWaitActivity',    '*')
+options.set_capability('appium:appWaitDuration', 30000)
+
+
+# Optional capabilities for stability
+options.set_capability('appium:noReset', False)
+options.set_capability('appium:skipDeviceInitialization', True)
+options.set_capability('appium:ignoreHiddenApiPolicyError', True)
+
+driver = webdriver.Remote('http://localhost:4723', options=options)
+
+print("Smart Things App Launched successfully!")
+
+#Navigation till home screen
+title_text = driver.find_element(By.XPATH, '//android.widget.TextView[@resource-id="com.samsung.android.oneconnect:id/introTitle"]').text
+print(title_text)
+driver.find_element(By.ID, 'com.samsung.android.oneconnect:id/intro_allow_button').click()
+driver.find_element(By.XPATH, f'//android.widget.Button[@text = "More"]').click()
+driver.find_element(By.XPATH, f'//android.widget.Button[@text = "Continue"]').click()
+time.sleep(25)
+driver.find_element(By.XPATH, f'//android.widget.Button[@text = "Start SmartThings"]').click()
+time.sleep(2)
+
+driver.find_element(By.XPATH, f'//*[contains(@text,"While using the app")]').click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]').click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]').click()
+
+home_title = driver.find_element(By.XPATH, '//android.widget.TextView[@resource-id="com.samsung.android.oneconnect:id/title"]').text
+print(home_title)
+
+#End of driver session
+time.sleep(5)
+driver.quit()
