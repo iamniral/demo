34e412debab1019ef02137e1d2e96f3c1dd6e590
iamniral <niral.shah@acldigital.com>
2026-06-11 11:30:10 +0530

    Unknown Chnages

---
+++ learing.py
@@ -0,0 +1,19 @@
+# Reverse string
+# def reverse(s):
+#     return s[::-1]
+#
+# print(reverse("hello world"))
+
+#String is palidrome
+# def is_palidrome(s):
+#     s = s.replace(" ", "").lower()
+#     return s == s[::-1]
+# print(is_palidrome("Race Car"))
+
+#Find duplicate element in the list
+
+l = [1,2,3,1]
+
+def duplicate(l):
+    cout = counter(l)
+    re
